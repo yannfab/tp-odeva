c1cf551d51b50f5bd4ec3bfdd1121391316fe054
Vandamme Lea <vandamme@a14p5.fil.univ-lille1.fr>
2018-10-19 08:49:08 +0200

    ajout d'une fonction dans Hello World

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,11 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+	System.out.println("Je change des trucs");
+    }
+
+    public String blaaablaaaablaaaa (){
+ 	return "Je dis des choses";
     }
 
 }
