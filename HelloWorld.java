7561e20ca4bca8bf5f88d7d3d43f07ed515962a4
Ben Jaafar Imen <benjaafar@a14p7.fil.univ-lille1.fr>
2018-10-19 08:49:54 +0200

    (ajout d'un getter et un setter )

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,13 @@
 public class HelloWorld {
-
+ String H;
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
     }
-
+public String getH() {
+		return H;
+	}
+	public void setH(String H) {
+		this.H = H;
+	}
 }
