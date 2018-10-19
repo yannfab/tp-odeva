2d582afc8a26bd55214ac2f428b99396702db3f1
Yete Hona <yete@a14p6.fil.univ-lille1.fr>
2018-10-19 08:48:25 +0200

    ajout d'une methode (pour rechercher un contact)

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,3 +6,31 @@ public class HelloWorld {
     }
 
 }
+
+//un peu de code java
+
+public Contact rechercherContact (String nom) throws SQLException{
+		
+	      Connection conn = DriverManager.getConnection(url, user, passwd);
+	         
+	      Statement state = conn.createStatement();
+	      
+	      PreparedStatement ps=conn.prepareStatement("Select * from contact where nom=?");
+	      ps.setString(1, nom);
+	      ResultSet rs=ps.executeQuery();
+	      
+	      while(rs.next()!=false){
+		      rs.next();
+		      String nm=rs.getString(2);
+		      String pn=rs.getString(3);
+		      int t=rs.getInt(4);
+		      int id = rs.getInt(1);
+		      
+		      if (nm == nom)
+		    	res = new  Contact (id,t,nm,pn);
+	      }
+		     
+		      return res;     
+		   
+	}
+
